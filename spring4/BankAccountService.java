18486a635c1a829e8d42aaac95ce8cc83caca8af
Adityasai-github <92909779+Adityasai-github@users.noreply.github.com>
2021-11-11 18:21:38 +0530

    Add files via upload

---
+++ spring4/BankAccountService.java
@@ -0,0 +1,8 @@
+package spring;
+
+public interface BankAccountService {
+	public double withdraw(long accountld, double balance);
+	public double deposit(long accountld, double balance);
+	public double getBalance(long accountld);
+	public boolean fundTransfer(long fromAccount, long toAccount, double amont);
+}
